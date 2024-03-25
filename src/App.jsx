77bdc6a58ea4d1a6ba257bcf87fa83bf35d0f3e6
it27427 <istiak.tushar.it@gmail.com>
2024-03-25 19:14:50 +0600

    adding pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import Header from './components/Header';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
   return (
     <>
-      {/* HEADER-SECTION */}
-      <Header />
-
-      {/* HERO-SECTION */}
-      <Hero />
-
-      {/* DEVELOPERS AND EMPLOYERS */}
-      <HomeCards />
-
-      {/* BROWSE-JOBS */}
-      <JobListings />
-
-      {/* BROWSE-ALL-JOBS */}
-      <ViewAllJobs />
+      <RouterProvider router={router} />
     </>
   );
 };
