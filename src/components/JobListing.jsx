a848b0a804ea975f9118d75897e81991886e0e4f
it27427 <istiak.tushar.it@gmail.com>
2024-03-25 18:05:23 +0600

    job-descriptions toggle done and react-icons added

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
 
 const JobListing = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + '...';
+  }
+
   return (
     <div className='bg-white rounded-xl shadow-md relative'>
       <div className='p-4'>
@@ -9,9 +17,14 @@ const JobListing = ({ job }) => {
           <h3 className='text-xl font-bold'>{job.title}</h3>
         </hgroup>
 
-        <div className='min-h-40 max-h-48'>
-          <p className='mb-5'>{job.description}</p>
-        </div>
+        <p className='mb-5'>{description}</p>
+
+        <button
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          className='text-indigo-500 mb-5 hover:text-indigo-700'
+        >
+          {showFullDescription ? 'Show Less' : 'Show More'}
+        </button>
 
         <h3 className='text-indigo-500 mb-2'>{job.salary} / Year</h3>
 
