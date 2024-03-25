9de74fe3e017b4aa0d807a46764e23f5017805d3
it27427 <istiak.tushar.it@gmail.com>
2024-03-25 17:27:17 +0600

    JobListing Data Dynamically Updated

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,24 +8,27 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {jobs.map(({ id, type, title, description, salary, location }) => (
-            <div className='bg-white rounded-xl shadow-md relative' key={id}>
+          {jobs.map((job) => (
+            <div
+              className='bg-white rounded-xl shadow-md relative'
+              key={job.id}
+            >
               <div className='p-4'>
                 <div className='mb-6'>
-                  <div className='text-gray-600 my-2'>{type}</div>
-                  <h3 className='text-xl font-bold'>{title}</h3>
+                  <div className='text-gray-600 my-2'>{job.type}</div>
+                  <h3 className='text-xl font-bold'>{job.title}</h3>
                 </div>
 
-                <div className='mb-5'>{description}</div>
+                <div className='mb-5'>{job.description}</div>
 
-                <h3 className='text-indigo-500 mb-2'>{salary}</h3>
+                <h3 className='text-indigo-500 mb-2'>{job.salary} / Year</h3>
 
                 <div className='border border-gray-100 mb-5'></div>
 
                 <div className='flex flex-col lg:flex-row justify-between mb-4'>
                   <div className='text-orange-700 mb-3'>
                     <i className='fa-solid fa-location-dot text-lg'></i>
-                    {location}
+                    {job.location}
                   </div>
                   <a
                     href='job.html'
