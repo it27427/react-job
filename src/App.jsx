94edd26c6d522c339adc25fe6c037575c18eceb3
it27427 <istiak.tushar.it@gmail.com>
2024-03-25 19:21:49 +0600

    Navbar and All Links Updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import {
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import AddJobPage from './pages/AddJobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJobPage />} />
     </Route>
   )
 );
