d67f48a1163b2ad73a6cc09b03c5330dd0bca7a3
it27427 <istiak.tushar.it@gmail.com>
2024-03-26 16:28:19 +0600

    edit-job form handling

--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -37,7 +37,7 @@ const EditJobPage = ({ updateJobSubmit }) => {
 
     toast.success('Job Updated Successfully!');
 
-    return navigate('/jobs');
+    return navigate(`/jobs/${id}`);
   };
 
   return (
